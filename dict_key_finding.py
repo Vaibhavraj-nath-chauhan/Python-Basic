fd69480db352b59cc1150495f93d2642fd676210
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2021-05-06 10:28:04 +0530

    Basic

---
+++ dict_key_finding.py
@@ -0,0 +1,12 @@
+sample_data = {
+    "class":{
+        "student":{
+            "name":"Mike",
+            "marks":{
+                "physics":70,
+                "history":80
+            }
+        }
+    }
+}
+print(sample_data["class"]["student"]["marks"]["history"])
