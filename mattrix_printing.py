00bcb1b657d78c6151a03d4cf55ba324dd6cd598
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2021-05-20 19:50:57 +0530

    Basic

---
+++ mattrix_printing.py
@@ -0,0 +1,10 @@
+mat = []
+print("Createing a Mattrix(3X3), Enter values-->")
+for i in range(3):
+    k = []
+    for j in range(3):
+        user = int(input())
+        k.append(user)
+    print("\n")
+    mat.append(k)
+print(mat)
