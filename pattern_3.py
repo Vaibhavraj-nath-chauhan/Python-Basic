a68d279784a3fd72c6acb55bed8af459c694dc6b
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2021-05-23 21:53:02 +0530

    Basic

---
+++ pattern_3.py
@@ -0,0 +1,8 @@
+user = int(input("Enter Values-->"))
+for i in range(user,0,-2):
+    for i in range(i):
+        if i%2==0:
+            print(1,end="")
+        else:
+            print(0,end="")
+    print("\n")
